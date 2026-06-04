4fb89cb1681d5c7ff0d744a8fdef6c00b0ad9b79
SmilingDogs <a.kudr74@gmail.com>
2026-06-04 16:19:19 +0300

    header text updates

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
 
   return (
     <div className="app">
-      <HeaderWithCounter turn={turn} />
+      <HeaderWithCounter turn={turn} sunk={won} />
       <Battlefield matrix={matrix} onFire={fire} disabled={false} won={won} />
       <ResetButton reset={reset} />
       <RussianWarship sunk={won} />
--- src/components/HeaderWithCounter.tsx
+++ src/components/HeaderWithCounter.tsx
@@ -1,10 +1,13 @@
 type HeaderProps = {
   turn: number;
+  sunk: boolean;
 };
 
-export const HeaderWithCounter = ({ turn }: HeaderProps) => (
-  <div>
-    <p>2 Enemy ships are hiding in the sea...</p>
-    <h2> Number of srikes: {turn}</h2>
-  </div>
-);
+export const HeaderWithCounter = ({ turn, sunk }: HeaderProps) => {
+  return (
+    <div>
+      <p>2 Enemy ships are {sunk ? `destroyed` : `hiding in the sea...`}</p>
+      <h2> Number of srikes: {turn}</h2>
+    </div>
+  );
+};
